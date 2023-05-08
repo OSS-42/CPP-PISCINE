07602f60784c345be092f4bad18afe3978804fd7
xDeadpoolx <eric.wursteisen@gmail.com>
2023-05-08 16:04:19 -0400

    update ex00

--- m05/ex00/src/Bureaucrat.cpp
+++ m05/ex00/src/Bureaucrat.cpp
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 14:50:35 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/05/08 15:19:55 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/05/08 15:48:29 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 // --------------- CANNON -----------------
 Bureaucrat::Bureaucrat (std::string name, int grade) : m_name(name), m_grade(grade) {
 	if (m_grade < 1)
-		throw Bureaucrat::gradeTooHighException();
+		throw Bureaucrat::GradeTooHighException();
 	else if (m_grade > 150)
-		throw Bureaucrat::gradeTooLowException();
+		throw Bureaucrat::GradeTooLowException();
 	else
 		std::cout << GRN << m_name << ", bureaucrat grade " << m_grade << " appeared." NC << std::endl;
 }
@@ -33,10 +33,6 @@ Bureaucrat::~Bureaucrat(void) {
 Bureaucrat& Bureaucrat::operator=(const Bureaucrat& rhs) {
 	m_grade = rhs.m_grade;
 	return (*this);
-}
-
-void Bureaucrat::operator<<(const Bureaucrat& rhs) {
-	std::cout << YEL << rhs.m_name << ", bureaucrat grade " << rhs.m_grade << NC << std::endl;
 }
 
 // ------------ MEMBER FUNCTION --------------
@@ -66,10 +62,7 @@ void	Bureaucrat::gradeDown (void) {
 	}
 }
 
-void	Bureaucrat::gradeTooHighException(void) {
-	std::cerr << MAG "Can't increase Grade." NC << std::endl;
-}
-
-void	Bureaucrat::gradeTooLowException(void) {
-	std::cerr << MAG "Can't decrease Grade." NC << std::endl;
+// ------------ OTHER --------------
+std::ostream& operator<<(std::ostream& o, const Bureaucrat& value) {
+	return (o << MAG << value.getName() << ", Bureaucrat of grade " << value.getGrade() << NC << std::endl);
 }
--- m05/ex00/src/main.cpp
+++ m05/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 10:57:18 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/05/08 12:58:28 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/05/08 16:03:39 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,63 +14,75 @@
 
 int	main(void) {
 
+	std::cout << std::endl;
+	std::cout << "--- Joe ---" << std::endl;
 	try {
 		Bureaucrat Joe ("Joe", 42);
 		std::cout << std::endl;
 	
 		Joe.gradeDown();
 		Joe.gradeUp();
+		std::cout << Joe << std::endl;
 		std::cout << std::endl;
 	}
-	catch(std::exception e) {
-		std::cerr << MAG << e.what() << ": Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
+	catch(const std::exception& e) {
+		std::cerr << MAG << e.what() << " Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
 	}
 	
 	std::cout << std::endl;
+	std::cout << "--- Will ---" << std::endl;
 	try {
 		Bureaucrat Will ("Will", -42);
 		std::cout << std::endl;
 		Will.gradeUp();
+		std::cout << Will << std::endl;
 		std::cout << std::endl;
 	}
-	catch(std::exception e) {
-		std::cerr << MAG << e.what() << ": Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
+	catch(const std::exception& e) {
+		std::cerr << MAG << e.what() << " Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
 	}
 	
 	std::cout << std::endl;
+	std::cout << "--- Jack ---" << std::endl;
 	try {
 		Bureaucrat Jack ("Jack", 150);
 		std::cout << std::endl;
 		Jack.gradeDown();
 		Jack.gradeUp();
+		std::cout << Jack << std::endl;
 		std::cout << std::endl;
 	}
-	catch(std::exception e) {
-		std::cerr << MAG << e.what() << ": Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
+	catch(const std::exception& e) {
+		std::cerr << MAG << e.what() << " Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
 	}
 	
 	std::cout << std::endl;
+	std::cout << "--- Avarell ---" << std::endl;
 	try {
 		Bureaucrat Avarell ("Avarell", 151);
 		std::cout << std::endl;
 		Avarell.gradeUp();
 		Avarell.gradeDown();
+		std::cout << Avarell << std::endl;
 		std::cout << std::endl;
 	}
-	catch(std::exception e) {
-		std::cerr << MAG << e.what() << ": Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
+	catch(const std::exception& e) {
+		std::cerr << MAG << e.what() << " Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
 	}
 	
 	std::cout << std::endl;
+	std::cout << "--- Mama Dalton ---" << std::endl;
 	try {
 		Bureaucrat Mama ("Mama Dalton", 1);
 		std::cout << std::endl;
-		Mama.gradeUp();
 		Mama.gradeDown();
+		Mama.gradeUp();
+		std::cout << Mama << std::endl;
+		Mama.gradeUp();
 		std::cout << std::endl;
 	}
-	catch(std::exception e) {
-		std::cerr << MAG << e.what() << ": Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
+	catch(const std::exception& e) {
+		std::cerr << MAG << e.what() << " Invalid Parameters, cannot instanciate the bureaucrat." NC << std::endl;
 	}
 
 	return 0;
