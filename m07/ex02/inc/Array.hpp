eb013df72eca486fc790ac9ffe3b9d1ee7c3a923
OSS-42 <eric.wursteisen@gmail.com>
2023-05-23 13:24:54 -0400

    update m07/ex02

--- m07/ex02/inc/Array.hpp
+++ m07/ex02/inc/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 10:58:45 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/05/19 18:32:43 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/05/23 13:18:21 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,55 +27,34 @@
 # include <string>
 
 template<typename T>
-class Array<T> {
+class Array {
 	public:
-		Array() 							{ m_array = new T [0]; }
-		Array(unsigned int n)				{ m_array = new T [n]; }
-		Array(const Array& other)			{ m_array = new T [other.size()];
-											  for (size_t i = 0; i < other.size(); i++) {this->m_array[i] = other.m_array[i];} }
-		Array& operator=(const Array& rhs)	{ *m_array = *rhs.m_array; return *this;}
+		Array() : m_array (new T[0]), m_size(0)					{ }
+		Array(unsigned int n) : m_array(new T[n]), m_size(n)	{ }
+		Array(const Array& other) : m_array(new T [other.m_size]), m_size(other.m_size)	{
+												for (size_t i = 0; i < other.size(); i++) {
+												this->m_array[i] = other.m_array[i];} 
+		}
+
+		Array& operator=(const Array& rhs)	{ 
+								if (this != &rhs) {
+									delete [] m_array;
+									m_size = rhs.size;
+									m_array = new T [m_size];
+									for (size_t i = 0; i < other.size(); i++) {
+										m_array[i] = rhs.m_array[i];
+										}
+									}
+								return (*this);
+		}
+
 		~Array()							{ delete [] m_array; }
 
-		const unsigned_int size() const		{ return }
+		unsigned int size() const		{ return m_size; }
 		
 	private:
-		T*	m_array;
+		T*				m_array;
+		unsigned int	m_size;
 };
 
 #endif
-
-
-
-// template<typename T>
-// class Array {
-// 	public:
-// 		Array() : m_array(new T [0]), m_size(0) { }
-
-// 		Array(unsigned int n) : m_array(new T [n]), m_size(n) { }
-
-// 		Array(const Array& other) : m_array(new T [other.m_size]), m_size(other.m_size) {
-// 			for (size_t i = 0; i < other.m_size; i++) {
-// 				this->m_array[i] = other.m_array[i];
-// 			}
-// 		}
-
-// 		Array& operator=(const Array& rhs) {
-// 			if (this != &rhs) { // protect against invalid self-assignment
-// 				delete[] m_array; // delete old array
-// 				m_size = rhs.m_size;
-// 				m_array = new T[m_size]; // create new array
-// 				for (size_t i = 0; i < m_size; i++) {
-// 					m_array[i] = rhs.m_array[i]; // copy elements
-// 				}
-// 			}
-// 			return *this;
-// 		}
-
-// 		~Array() { delete [] m_array; }
-
-// 		size_t size() const { return m_size; }
-		
-// 	private:
-// 		T*	m_array;
-// 		size_t m_size;
-// };
--- m07/ex02/src/main.cpp
+++ m07/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 10:57:18 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/05/19 17:59:05 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/05/23 08:56:12 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,38 +20,10 @@ try {
 
 
 	std::cout << MAG "---- INITIAL TABLES ----" NC << std::endl;
-	for (int i = 0; i < 5; i++) {
-		std::cout << tableA[i];
-		if (i != 4)
-			std::cout << ", ";
-	}
-	std::cout << std::endl;
-	
-	for (int i = 0; i < 10; i++) {
-		std::cout << tableB[i];
-		if (i != 10)
-			std::cout << ", ";
-	}
-	std::cout << "\n" << std::endl;
 
-	std::cout << CYN "calling iter()" NC << std::endl;
-	::iter(tableA, 5, &addOne);
-	::iter(tableB, 10, &addString);
-	std::cout << std::endl;
 	
 	std::cout << MAG "---- NEW TABLES ----" NC << std::endl;
-	for (int i = 0; i < 5; i++) {
-		std::cout << tableA[i];
-		if (i != 4)
-			std::cout << ", ";
-	}
-	std::cout << std::endl;
 	
-	for (int i = 0; i < 10; i++) {
-		std::cout << tableB[i];
-		if (i != 10)
-			std::cout << ", ";
-	}
 
 	return 0;
 }
