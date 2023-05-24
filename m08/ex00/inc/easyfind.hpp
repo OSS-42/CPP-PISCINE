fc9ed9678b6dfa65f40b69926ecaa9280b6823dc
OSS-42 <eric.wursteisen@gmail.com>
2023-05-24 13:44:39 -0400

    creation m08

---
+++ m08/ex00/inc/easyfind.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 10:58:45 by ewurstei          #+#    #+#             */
+/*   Updated: 2023/05/24 13:44:10 by ewurstei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+// COLOURS CODES
+# define NC "\e[0m"
+# define RED "\e[0;31m"
+# define GRN "\e[0;32m"
+# define YEL "\e[0;33m"
+# define MAG "\e[0;35m"
+# define CYN "\e[0;36m"
+# define REDB "\e[41m"
+
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <algorithm>
+
+template<typename T>
+void easyfind(T& lhs, int num) {
+	std::set<int> 
+	std::cout << GRN "swap done" NC << std::endl;
+}
+
+template<typename T>
+const T min(T lhs, T rhs) {
+	return ((lhs <= rhs) ? lhs : rhs);
+}
+
+template<typename T>
+const T max(T lhs, T rhs) {
+	return ((lhs >= rhs) ? lhs : rhs);
+}
+
+#endif
