b00dfc88ff7dabc3dab16adb3f5bad2b6f361618
OSS-42 <eric.wursteisen@gmail.com>
2023-07-19 15:33:49 -0400

    update m09 c02

--- m09/ex02/src/main.cpp
+++ m09/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ewurstei <ewurstei@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 10:57:18 by ewurstei          #+#    #+#             */
-/*   Updated: 2023/07/19 15:02:39 by ewurstei         ###   ########.fr       */
+/*   Updated: 2023/07/19 15:30:44 by ewurstei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,21 @@ int main(int argc, char **argv) {
 		std::cout << RED "Error: Usage: ./PmergeMe \"values\" (min 2)" NC << std::endl;
 		return 0;
 	} else {
-		long idx = 1;
-		std::list<double>	arglist;
-		
-		while (idx < argc) {
-			std::string arg = argv[idx];
-			if (arg.find_first_not_of("0123456789") != std::string::npos)
-				throw std::runtime_error("Invalid input value");
-			else {
-				arglist.push_back(std::stod(arg));
-			}
-			idx++;
-		}
-
 		try {
-			PmergeMe(arglist);
-		}
-
-		catch (std::exception& e) {
+			long idx = 1;
+			std::list<double>	arglist;
+			
+			while (idx < argc) {
+				std::string arg = argv[idx];
+				if (arg.find_first_not_of("0123456789") != std::string::npos)
+					throw std::runtime_error("Invalid input value");
+				else {
+					arglist.push_back(std::stod(arg));
+				}
+				idx++;
+			} 
+				PmergeMe sort(arglist);
+		} catch (std::exception& e) {
 			std::cout << RED ">>> Error : " << e.what() << " <<<" NC << std::endl;
 		}
 	}
